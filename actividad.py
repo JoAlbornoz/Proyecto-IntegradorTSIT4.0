475e48f48e7f4872fae36334d2760e90939bfb7d
Gustavo Godoy <35662736+chulkx@users.noreply.github.com>
2022-06-23 20:39:42 -0300

    Update actividad.py
    Comentarios con lo solicitado por la Scrum Master para cada Funcion.

--- actividad.py
+++ actividad.py
@@ -1,1 +1,60 @@
-print("Ingresar números")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#==================
+#Funcion ingreso-lista
+'''
+Pedir al usuario que ingrese 5 valores enteros
+Creación de lista.
+'''
+
+
+
+
+#==================
+#Funcion Suma
+'''
+Recibe como parámetro la lista
+Devuelve la suma total de todos sus elementos.
+'''
+
+
+
+
+
+#==================
+#Funcion Promedio
+'''
+Recibe como parámetro la lista
+devuelve el promedio de sus elementos.
+'''
+
+
+
+
+
+#==================
+#Funcion Maximo
+'''
+Recibe como parámetro la lista
+devuelve el valor máximo de todos los elementos que contiene.
+'''
+
+
+
+
+
+#==================
+#Funcion Minimo
+'''
+Recibe como parámetro la lista
+devuelve el valor mínimo de todos los elementos que contiene.
+'''
+
+
+
+
+
+#==================
+#Funcion Principal
